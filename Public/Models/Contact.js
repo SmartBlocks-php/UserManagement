8cf50aa5cf6c47d459f66e60de87cfccc548c50f
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-12-02 00:19:25 +0100

    * add real contact to contact model.

--- Public/Models/Contact.js
+++ Public/Models/Contact.js
@@ -3,11 +3,11 @@ define([
     'backbone'
 ], function (_, Backbone) {
     var Model = Backbone.Model.extend({
-        default: {
+        default:{
 
         },
-        urlRoot: "/UserManagement/Contacts",
-        parse: function (response) {
+        urlRoot:"/UserManagement/Contacts",
+        parse:function (response) {
 
             var user_a_array = response.user_a;
             var user_b_array = response.user_b;
@@ -15,8 +15,10 @@ define([
             var user_a = SmartBlocks.Blocks.Kernel.Data.users.get(user_a_array['id']);
             var user_b = SmartBlocks.Blocks.Kernel.Data.users.get(user_b_array['id']);
 
+            SmartBlocks.current_user.get("id")
             response.user_a = user_a;
             response.user_b = user_b;
+            response.real_contact = SmartBlocks.current_user.get("id") == user_a_array['id'] ? user_b : user_a;
 
             return response;
         }
